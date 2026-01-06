66734179f0c28f14772bb226ffa3820d477c423e
Ali Reza Habibi <shahreyarhabibi@gmail.com>
2026-01-06 10:14:40 +0430

    Dashboard and Sidebar Reponsiveness

--- components/dashboard/BudgetChart.js
+++ components/dashboard/BudgetChart.js
@@ -1,12 +1,5 @@
 "use client";
-import {
-  PieChart,
-  Pie,
-  Cell,
-  Legend,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 export default function BudgetChart() {
   const data = [
@@ -16,11 +9,13 @@ export default function BudgetChart() {
     { name: "Mobile", value: 50 },
   ];
 
-  // Colors for each slice
-  const COLORS = ["#104e64", "skyblue", "darkorange", "#721378 "];
+  const COLORS = ["#104e64", "skyblue", "darkorange", "#721378"];
+
+  const spent = 192.0;
+  const limit = 975.0;
 
   return (
-    <div style={{ width: "100%", maxWidth: "300px", height: "300px" }}>
+    <div style={{ width: "100%", maxWidth: "400px", height: "300px" }}>
       <ResponsiveContainer>
         <PieChart>
           <Pie
@@ -32,13 +27,34 @@ export default function BudgetChart() {
             cornerRadius={10}
             stroke="none"
           >
-            {data.map((entry, index) => (
+            {data.map((_, index) => (
               <Cell
                 key={`cell-${index}`}
                 fill={COLORS[index % COLORS.length]}
               />
             ))}
           </Pie>
+
+          {/* Center Text */}
+          <text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle">
+            <tspan
+              x="50%"
+              dy="-0.3em"
+              fontSize="35"
+              fontWeight="700"
+              style={{ fill: "var(--color-foreground)" }}
+            >
+              ${spent.toFixed(1)}
+            </tspan>
+            <tspan
+              x="50%"
+              dy="2em"
+              fontSize="14"
+              style={{ fill: "var(--color-text)" }}
+            >
+              of ${limit.toFixed(1)} limit
+            </tspan>
+          </text>
         </PieChart>
       </ResponsiveContainer>
     </div>
--- components/dashboard/BudgetSection.js
+++ components/dashboard/BudgetSection.js
@@ -7,13 +7,13 @@ export default function BudgetSection({ budgetCategories }) {
     <div className="flex flex-col text-foreground bg-background shadow-xl dark:bg-linear-45 dark:from-background dark:to-primary/20 border border-text/10 p-6 gap-5 rounded-2xl">
       <SectionHeader title="Budget" linkHref="/budget" />
 
-      <div className="flex items-center justify-between gap-2">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-2">
         <div className="w-85.5">
           <BudgetChart />
         </div>
 
         {/* Budget Categories */}
-        <div className="flex flex-col gap-2 justify-center">
+        <div className="md:flex md:flex-col grid grid-cols-2 grid-rows-2 gap-2 justify-center">
           {budgetCategories.map((category) => (
             <PotItem key={category.name} {...category} />
           ))}
